0b59e259a55f3818d900b1c814274a2a8428d99c
Sanchit-Srivastava <Sanchit-Srivastava@users.noreply.github.com>
2026-02-25 00:54:50 +0000

    Deploying to gh-pages from @ Sanchit-Srivastava/sanchit-srivastava.github.io@a2d6eb2b318bd92687f8d47f5ea823278afc382e 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Sanchit-Srivastava/Sanchit-Srivastava.github.io",
+    "data-repo": "Sanchit-Srivastava/sanchit-srivastava.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
